ec6dcd5bd6c18bddb83294b9a6120ca3fe5a676f
Phillip <phillipwu95@gmail.com>
2019-10-12 18:27:34 +1100

    02 implemented test and solution

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,40 @@
 #write your code here
+#
+
+def add(a,b)
+	return a+b
+end
+
+def subtract(a,b)
+	return a-b
+end
+
+def sum(arr)
+	sum = 0
+	arr.each do |elem|
+		sum = sum + elem
+	end
+	return sum
+end
+
+def multiply(arr)
+	sum = 1
+	arr.each do |elem|
+		sum = sum * elem
+
+	end
+	return sum
+end
+
+def power(a,b)
+	return a**b
+end
+
+def factorial(n)
+	if (n==0 or n==1)
+		return 1
+	else
+		return n * factorial(n-1)
+	end
+end
+
