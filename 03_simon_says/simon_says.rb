8b95a6b7ab51a361a8ffe71bc9e238f5cc18b83a
Phillip <phillipwu95@gmail.com>
2019-10-12 18:48:31 +1100

    03 finished

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,42 @@
 #write your code here
+
+def echo(something)
+	return something
+end
+
+def shout(something)
+	something.upcase
+end
+
+def repeat(something, n=2)
+	return (something+" ")*(n-1) + something
+end
+
+def start_of_word(word, n)
+	return word[0..n-1]
+end
+
+def first_word(sentence)
+	sentence.split(" ")[0]
+end
+
+def titleize(sentence)
+	arr = sentence.split(" ")
+	if(small_word(arr[0]))
+		arr[0] = arr[0].capitalize
+	end
+	newSen = ""
+	arr.each do |word|
+		if(small_word(word))
+			newSen = newSen + word + " "
+			next
+		end
+		newSen = newSen + word.capitalize + " "
+	end
+	return newSen[0..-2]
+end
+
+def small_word(word) 
+	smallWordArr = ["the","an","a","and","at","over"]
+	return smallWordArr.include? word
+end
