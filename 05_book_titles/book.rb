236bed545411b7f314b7f7bb13297a2cc5f8be16
Phillip <phillipwu95@gmail.com>
2019-10-19 15:57:09 +1100

    completed 05 book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
 # write your code here
+    def initialize()
+        @title = ""
+    end
+
+    def title()
+        return capitalize(@title)
+    end
+
+    def title=(title)
+        @title = title
+    end
+
+    def capitalize(sentence)
+        adverbs = ["a","an","and","in","the","of","for","to","at"]
+        words = sentence.split(/,|\s/)
+        words.each_with_index do |word, i|
+            if(i==0 or !adverbs.include?(word))
+                words[i] = words[i].capitalize
+            end
+        end
+        return words.join(" ")
+    end            
 end
